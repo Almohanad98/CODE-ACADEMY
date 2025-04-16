bb5b592362f26d78a402a84ad7423c0ab9ac383e
Almohanad Alfarei <163166821+Almohanad98@users.noreply.github.com>
2025-04-16 08:56:11 +0400

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 15 19:53:52 2025
+
+@author: almohanadalfarei
+"""
+
+
+############1
+'''
+name= 'almohanad'
+age=25
+height=182.0
+
+print('hello , my name is ',name,'I am', age,'years old and my geight is',height,'meters.')
+'''
+
+##########2
+'''
+number1= float (input('this is the first number:'))
+number2=float (input('this is the second number:'))
+
+add= number1 + number2
+subtracts= (number1) - (number2)
+multiplis= number1 * number2
+divides= number1 / number2
+print('addition:',add)
+print('subtraction:',subtracts)
+print('Multiplication:', multiplis)
+print('Division:',divides)
+'''
+######################4
+'''
+length = float(input(' enter your lentgh:'))
+width= float(input(' enter your width:'))
+area = length * width
+print(' the area of the rectangel is',area)
+'''
+###################
